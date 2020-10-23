e53ec65943ab7614f4e7b414defca4a96536e516
Sigurd <j.skadborg00@gmail.com>
2020-10-23 19:06:20 +0200

    First try

--- lektion_7/afleveringsopgave6.c
+++ lektion_7/afleveringsopgave6.c
@@ -1,11 +1,45 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
 double trapz();
-void scan_data();
+double sum();
+double h();
 
 
 int main(void){ 
-    scan_data();
-    trapz(n);  
+    double (*fn) (int);
+    char f;
+    int a, b, n;
+    printf("Give me the limits a,b\n");
+    scanf(" %d %d", &a, &b);
+    printf("Now give me a number for the fineness and a char for the function\n");
+    scanf(" %d %c", &n, &f);
+    if( f == 'h')
+        fn = &h;
+
+    printf("The area is calculated to be = %lf",trapz(a, b, n, fn) ); 
+    
+    return EXIT_SUCCESS;
+}
+
+double trapz(int a, int b, int n, double (*fn) (int)){
+    double h = (b - a) / n;
+
+    double T =  h/2 * (fn(a) + fn(b) + 2*(sum(n, fn)));
+
+    return T;
+}
+
+
+double sum(int n, double (*fn) (int)){
+    double sum = 0.0;
+    int i;
+    for(i = 1; i < n; i++)
+        sum += fn(n);
+    return sum;
+}
+
+double h(int x){
+    return sqrt(4-(x*x)); 
 }
