3dc0072c57b84d3d7646b4e2e7870ade1a230eec
Sigurd <j.skadborg00@gmail.com>
2020-11-08 15:50:48 +0100

    Fixed problems with reversing string

--- lektion_10/opg10_1.c
+++ lektion_10/opg10_1.c
@@ -2,16 +2,16 @@
 #include <stdio.h>
 #include <string.h>
 
-int *stringcopy(char *);
+char *stringrev(char *);
 
 int main(void){
     char string[100];
-    int *reversed;
+    char *reversed;
     printf("Type in a string you want to reverse\n");
     scanf(" %s", string);
-    reversed = stringcopy(string);
+    reversed = stringrev(string);
 
-    printf("The reversed string is: \n %s", reversed);
+    printf("The reversed string is:\n%s\n", reversed);
 
     free(reversed);
 
@@ -19,12 +19,13 @@ int main(void){
 }
 
 /*Not working*/
-int *stringcopy(char *string){
-    char * reversedStr = (char *) malloc(strlen(string) * sizeof(char));
+char *stringrev(char *string){
+    char *reversedStr = (char *) malloc(strlen(string) * sizeof(char));
     int i, j;
-    for(i = 0, j = strlen(string); i < strlen(string); i++, j--){
+    for(i = 0, j = strlen(string) -1; i < strlen(string); i++, j--){
         reversedStr[i] = string[j];
     }
+    reversedStr[i] = '\0';
     return reversedStr;
 }
 
