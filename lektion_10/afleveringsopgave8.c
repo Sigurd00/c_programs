faf5ad7236638130738b61bee9b37a9e8023cb06
Sigurd <j.skadborg00@gmail.com>
2020-11-17 08:43:52 +0100

    Niceness

--- lektion_10/afleveringsopgave8.c
+++ lektion_10/afleveringsopgave8.c
@@ -6,17 +6,16 @@
 void process_product_code(char *, char *, char *, char *);
 
 int main(void){
-    char productCode[] = "ATL1203S14";            /*Input*/
-    char warehouse[4]  = {};
-    char product[5]    = {};
-    char qualifiers[4] = {}; /*Output*/
+    char productCode[] = "ATL1203S14"; /*Input*/
+    char warehouse[100]  = ""; /*Output*/
+    char product[100]    = ""; /*Output*/
+    char qualifiers[100] = ""; /*Output*/
 
     printf("Product code is : %s\n", productCode);
     process_product_code(productCode, warehouse, product, qualifiers);
     printf("Warehouse: %s\nProduct: %s\nQualifiers: %s\n", warehouse, product, qualifiers);
     return EXIT_SUCCESS;
 }
-
 
 void process_product_code(char *productCode, char *warehouse, char *product, char *qualifiers){
     int i = 0, firstDigitPosition, alphaAfterDigitPosition = 0, checkForAlpha = 0;
